f80a6b6fd6d16396f04aad4698e097d511724776
zhujuntao <juntao.zhu@pactera.com>
2020-04-16 17:17:34 +0800

    合并分支dev内容

--- app/src/main/java/com/example/keytest/MainActivity.java
+++ app/src/main/java/com/example/keytest/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //添加一句话
+        //合并内容到master
 
     }
 }
